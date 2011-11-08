be0198b523cda333960dda63319e19b0232e32b3
Chris Umbel <chris@chrisumbel.com>
2011-11-08 16:34:49 -0500

    added sums. need a codebase-wide reformat

--- spec/matrix_spec.js
+++ spec/matrix_spec.js
@@ -3,6 +3,11 @@ var sylvester = require('../lib/node-sylvester'),
 Matrix = sylvester.Matrix;
 
 describe('matrix', function() {
+    it('should create a 1\'s matrix', function() {
+	var A = Matrix.One(2, 3);
+	expect(A).toEqual($M([[1,1,1], [1,1,1]]));
+    });
+
     it('should log', function() {
 	var A = Matrix.create([[1, 2, 3], [4, 5, 6]]);
 	expect(A.log()).toEqual($M([[0, 0.6931471805599453, 1.0986122886681098],
