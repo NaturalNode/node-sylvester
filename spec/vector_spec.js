22f22832b564cc54f7118a048de37056fff42289
Chris Umbel <chris@chrisumbel.com>
2011-11-08 23:40:43 -0500

    formatting, tests

--- spec/vector_spec.js
+++ spec/vector_spec.js
@@ -2,16 +2,28 @@
 var sylvester = require('../lib/node-sylvester'),
 Vector = sylvester.Vector;
 
+var x = Vector.create([1, 2, 3]);
+
 describe('vector', function() {
     it('should log', function() {
-	var x = Vector.create([1, 2, 3]);
 	expect(x.log()).toEqual($V([0, 0.6931471805599453, 1.0986122886681098]));
     });
 
     it('should dot product', function() {
-	var x = Vector.create([1, 2, 3]);
 	expect(x.dot(Vector.create([2, 3, 4]))).toBe(20);
     });
+
+    it('should support removal of head positions', function() {
+	expect(x.chomp(1)).toEqual($V([2, 3]));
+    });
+
+    it('should sum', function() {
+	expect(x.sum()).toBe(6);
+    });
+
+    it('should support addition of elements on the right side', function() {
+	expect(x.augment([4, 5])).toEqual($V([1, 2, 3, 4, 5]));
+    })
 
     it('should create with $V', function() {
 	var a = $V([2, 3, 4]);
